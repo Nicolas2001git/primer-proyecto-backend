a3dfe485cb2d5bcb219c38a28e9d6d16f2f2fe06
Nicolas Riveira <riveiranicolas@gmail.com>
2026-02-18 09:13:00 -0300

    Cambios menores

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import fs from "fs";
 const app = express();
 const port = 8080;
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log("Servidor Activo: " + port);
